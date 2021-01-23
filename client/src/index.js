f4e3d1c5772d8263ace2bd30385e2a180567b796
risan <potallegta@gmail.com>
2021-01-23 12:17:51 +0700

    First commit: client

---
+++ client/src/index.js
@@ -0,0 +1,8 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+function Test() {
+  return <h4>This is just a test</h4>
+}
+
+ReactDOM.render(<Test/>, document.getElementById('root'))
