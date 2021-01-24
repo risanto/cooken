ac3a55a347bd66036468d1bd3a7787bbd4342c6f
risan <potallegta@gmail.com>
2021-01-24 17:45:14 +0700

    Finished responsive layout for both mobile & desktop landing page. Next: implement search and make cards clickable

--- client/src/components/ImageCard.js
+++ client/src/components/ImageCard.js
@@ -2,9 +2,9 @@ import React from 'react'
 
 const ImageCard = ({ imageSrc, text }) => {
     return (
-        <div className="">
+        <div className="p-2 md:p-4 w-1/2 md:w-1/3">
             <img className="rounded-t-lg" src={imageSrc} />
-            <p className="bg-red-500 text-xs text-white text-center p-2 font-bold rounded-b-xl">{text}</p>
+            <p className="block bg-red-500 text-xs text-white text-center py-1 md:py-2 px-3 font-bold rounded-b-xl truncate">{text}</p>
         </div>
     )
 }
--- client/src/views/Home.js
+++ client/src/views/Home.js
@@ -6,29 +6,46 @@ import ImageCard from '../components/ImageCard'
 const Home = () => {
     const { randomRecipes, generateNewRandomRecipes } = useContext(RecipeContext)
 
-    const randomRecipesCards = randomRecipes.map(recipe => <ImageCard imageSrc={recipe.image} text={recipe.title.length < 19
-        ? recipe.title.length
-        : recipe.title.substring(0, 19) + '..'} />)
+    const randomRecipesCards = randomRecipes.map(recipe => <ImageCard imageSrc={recipe.image} text={recipe.title} />)
 
     return (
-        <div className="container">
-            <section className="flex justify-center align-center mt-8">
-                <img className="w-24" src="cooken-logo-2.png" alt="cooken-logo" />
+        <div className="container lg:px-32 lg:py-8 pb-8 mb-8">
+            <section className="md:hidden">
+                <div className="flex justify-center align-center mt-8">
+                    <img className="w-24" src="cooken-logo-2.png" alt="Cooken" />
+                </div>
+                <div className="flex justify-center align-center mt-4 px-2">
+                    <SearchBar className="flex-grow" />
+                </div>
             </section>
-            <section className="flex justify-center align-center mt-4">
-                <SearchBar />
+            <section className="hidden md:block">
+                <div className="flex justify-between align-center mt-8">
+                    <img className="w-24" src="cooken-logo-2.png" alt="Cooken" />
+                    <div className="flex justify-between align-center py-4">
+                        <SearchBar className="" />
+                    </div>
+                </div>
             </section>
-            <section className="grid grid-cols-2 mt-4 gap-5">
+
+            <p className="text-center text-gray-500 mt-4">Some recipes to inspire you.</p>
+
+            <section className="flex flex-wrap mt-2 align-center justify-center">
                 {randomRecipesCards}
             </section>
+
             <section className="flex justify-center align-center mt-4">
-                <button className="border-2 rounded-xl py-2 px-4 text-sm" onClick={generateNewRandomRecipes}>
-                    Show me other random recipes
+                <button className="flex align-center justify-center border-2 rounded-xl px-1 text-sm text-gray-500 hover:text-gray-600 hover:border-gray-400" onClick={generateNewRandomRecipes}>
+                    <p className="p-1">
+                        <img className="inline-block w-6" src="random-icon.png" />
+                        Show me other random recipes
+                    </p>
                 </button>
             </section>
-            <p className="text-center pt-4">Do you want to make a dish based on what's available on your fridge?</p>
-            <section className="flex justify-center align-center mt-2">
-                <button className="border-2 rounded-xl py-2 px-4 text-sm flex-grow-0 border-red-300 text-red-500">
+
+            <section className="flex flex-col md:flex-row justify-center align-center mt-3">
+                <p className="text-center text-sm py-4">Do you want to make a dish based on what's available on your fridge?</p>
+
+                <button className="border-2 rounded-xl py-2 px-4 mt-2 md:mt-0 md:ml-4 text-sm border-red-300 text-red-500 self-center hover:bg-red-500 hover:text-white">
                     Join / sign in
                 </button>
             </section>
