bca1e4557411b3354bcf372e354f3bfd2fcb187b
risan <potallegta@gmail.com>
2021-01-24 13:50:05 +0700

    Added ImageCard component

--- client/src/views/Home.js
+++ client/src/views/Home.js
@@ -5,13 +5,36 @@ import { RecipeContext } from '../contexts/Recipe'
 const Home = () => {
     const { randomRecipes, generateNewRandomRecipes } = useContext(RecipeContext)
 
+    // randomRecipes.forEach(recipe => {
+    //     console.log(recipe.title, recipe.image)
+    // })
+
+    const randomRecipesCards = randomRecipes.map(recipe => {
+        return (
+            <div className="">
+                <img className="rounded-t-lg" src={recipe.image} />
+                <p className="bg-red-500 text-xs text-white text-center p-2 font-bold rounded-b-xl">{
+                    recipe.title.length < 19
+                        ? recipe.title.length 
+                        : recipe.title.substring(0, 19) + '..' 
+                }</p>
+            </div>
+        )
+    })
+
     return (
         <div className="container">
             <div className="flex justify-center align-center pt-8">
-                <img className="w-24" src="cooken-logo-2.png" />
+                <img className="w-24" src="cooken-logo-2.png" alt="cooken-logo" />
             </div>
             <div className="flex justify-center align-center pt-8">
-                <SearchBar/>
+                <SearchBar />
+            </div>
+            <div className="grid grid-cols-2 pt-8 gap-5">
+                {randomRecipesCards}
+            </div>
+            <div className="flex justify-center align-center pt-4">
+                <button>Show me other random recipes</button>
             </div>
         </div>
     )
--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -2,9 +2,16 @@ module.exports = {
   purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
   darkMode: false, // or 'media' or 'class'
   theme: {
+    fontFamily: {
+      sans: ['Source Sans Pro', 'ui-sans-serif', 'system-ui']
+    },
     container: {
       center: true,
       padding: '2rem'
+    },
+    borderRadius: {
+      lg: '0.75rem',
+      xl: '1.25rem'
     },
     extend: {},
   },
