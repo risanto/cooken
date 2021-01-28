04ffd0dfd1086ab44d6b7f92df3c56a1b7fa4ee8
risan <potallegta@gmail.com>
2021-01-28 22:09:57 +0700

    Fix image not filling the whole card

--- client/src/components/ImageCard.js
+++ client/src/components/ImageCard.js
@@ -3,7 +3,7 @@ import React from 'react'
 const ImageCard = ({ imageSrc, text }) => {
     return (
         <div className="image-card shadow transform hover:-translate-y-2 cursor-pointer hover:shadow-xl m-2 md:m-4 w-28 md:w-1/4 rounded-t-lg rounded-b-xl">
-            <img className={"rounded-t-lg h-20"} src={imageSrc ? imageSrc : 'chef-hat-icon-wide.png'}/>
+            <img className={"rounded-t-lg"} src={imageSrc ? imageSrc : 'chef-hat-icon-wide.png'}/>
 
             {/* VERSION 1 */}
             <p className="block rounded-b-xl bg-red-500 text-xs text-white text-center py-1 md:py-2 px-3 font-bold truncate">{text}</p>
