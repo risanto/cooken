24dac1f805becce673813a7304c05e6e22f3a70c
risan <potallegta@gmail.com>
2021-02-03 21:18:38 +0700

    Create 2 nav versions

--- client/src/components/Nav.js
+++ client/src/components/Nav.js
@@ -1,21 +1,26 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 import SearchBar from './SearchBar'
 
 const Nav = () => {
     return (
         <>
-          <header className="flex flex-col mt-8 md:hidden">
-                <img className="self-center w-24 cursor-pointer" src="cooken-logo-2.png" alt="Cooken" />
+            <header className="flex flex-col mt-8 md:hidden">
+                <Link to="/" className="cursor-pointer place-self-center">
+                    <img className="self-center w-24 cursor-pointer" src="/cooken-logo-2.png" alt="Cooken" />
+                </Link>
                 <SearchBar className="self-center mt-4" />
             </header>
             <header className="hidden mx-8 mt-4 md:block">
                 <div className="flex justify-between align-center">
-                    <img className="w-24" src="cooken-logo-2.png" alt="Cooken" />
+                    <Link to="/" className="cursor-pointer place-self-center">
+                        <img className="w-24" src="/cooken-logo-2.png" alt="Cooken" />
+                    </Link>
                     <div className="flex justify-between py-4 align-center">
                         <SearchBar />
                     </div>
                 </div>
-            </header>  
+            </header>
         </>
     )
 }
