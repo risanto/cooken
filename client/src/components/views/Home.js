1bbeda38aaa3a1934fc9792c7d110cc53a353f02
risan <potallegta@gmail.com>
2021-02-06 11:06:34 +0700

    Add focus:outline-none to buttons

--- client/src/components/views/Home.js
+++ client/src/components/views/Home.js
@@ -29,7 +29,7 @@ const Home = () => {
             </section>
 
             <section className="flex justify-center mt-4 align-center">
-                <button className="flex justify-center px-1 text-sm text-gray-500 border-2 rounded-xl align-center hover:text-gray-600 hover:border-gray-400" onClick={generateNewRandomRecipes}>
+                <button className="flex justify-center px-1 text-sm text-gray-500 border-2 focus:outline-none rounded-xl align-center hover:text-gray-600 hover:border-gray-400" onClick={generateNewRandomRecipes}>
                     <p className="p-1">
                         <img className="inline-block w-6" src="random-icon.png" alt="random-icon" />
                         Show me other random recipes
@@ -40,7 +40,7 @@ const Home = () => {
             <section className="flex flex-col self-center justify-center w-3/4 mt-3 md:w-full md:flex-row align-center">
                 <p className="px-4 py-4 text-base text-center">Do you want to make a dish based on what's available on your fridge?</p>
 
-                <button className="self-center px-4 py-2 text-sm text-red-500 border-2 border-red-300 rounded-xl md:mt-0 bg-gradient-to-r hover:from-pink-600 hover:via-red-500 hover:to-red-600 hover:text-white">
+                <button className="self-center px-4 py-2 text-sm text-red-500 border-2 border-red-300 focus:outline-none rounded-xl md:mt-0 bg-gradient-to-r hover:from-pink-600 hover:via-red-500 hover:to-red-600 hover:text-white">
                     Join / sign in
                 </button>
             </section>
