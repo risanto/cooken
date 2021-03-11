dd1e25135e6a5413fed0ac966b122f41c3c95b70
risan <potallegta@gmail.com>
2021-03-11 15:40:28 +0700

    Change BrowserRouter to HashRouter to fix netlify 404 error on page refresh

--- client/src/index.js
+++ client/src/index.js
@@ -2,7 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import './index.css';
 import App from './App'
-import { BrowserRouter as Router } from 'react-router-dom'
+import { HashRouter as Router } from 'react-router-dom'
 
 function ReactApp() {
   return <App />
