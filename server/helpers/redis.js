3d57328ccac532ac1c02537ca6648cc9d53d33dd
risan <potallegta@gmail.com>
2021-03-14 08:44:19 +0700

    Minor modification on redis config

--- server/helpers/redis.js
+++ server/helpers/redis.js
@@ -1,8 +1,14 @@
 const redis = require('redis')
 const util = require('util')
-// const REDIS_PORT = process.env.REDIS_PORT
-// const redisClient = redis.createClient(REDIS_PORT)
-const redisClient = redis.createClient(process.env.REDIS_URL)
+
+let redisClient
+
+if (process.env.NODE_ENV === 'development') {
+    redisClient = redis.createClient(process.env.REDIS_PORT)
+} else {
+    redisClient = redis.createClient(process.env.REDIS_URL)
+}
+
 redisClient.get = util.promisify(redisClient.get)
 
 module.exports = { redisClient }
