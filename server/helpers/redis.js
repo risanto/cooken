7f758128e30ef54ceae6047ca68cceb572277323
risan <potallegta@gmail.com>
2021-03-14 08:39:01 +0700

    Configure redis setting

--- server/helpers/redis.js
+++ server/helpers/redis.js
@@ -1,7 +1,8 @@
 const redis = require('redis')
 const util = require('util')
-const REDIS_PORT = process.env.REDIS_PORT
-const redisClient = redis.createClient(REDIS_PORT)
+// const REDIS_PORT = process.env.REDIS_PORT
+// const redisClient = redis.createClient(REDIS_PORT)
+const redisClient = redis.createClient(process.env.REDIS_URL)
 redisClient.get = util.promisify(redisClient.get)
 
 module.exports = { redisClient }
