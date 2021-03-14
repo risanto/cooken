1b1af10841abcfb281676f87374ebdc8609dbd9b
risan <potallegta@gmail.com>
2021-03-14 12:40:52 +0700

    Add root route

--- server/routes/index.js
+++ server/routes/index.js
@@ -3,5 +3,8 @@ const router = require('express').Router()
 router.use('/recipes', require('./recipes'))
 router.use('/user', require('./user'))
 router.use('/savedRecipes', require('./savedRecipes'))
+router.get('/', (req, res) => {
+    res.send('Just initial endpoint, nothing going on here...')
+})
 
 module.exports = router
